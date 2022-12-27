453ce3ab7401398526a95db51fcf79327b9066ca
mikasa <470618507@qq.com>
2022-12-27 16:57:15 +0800

    feat: add functional 'network.checkIsInSameNetworkSegment'

--- src/utils.ts
+++ src/utils.ts
@@ -6,7 +6,19 @@ function isUndefined(val: any): val is undefined {
   return typeof val === 'undefined'
 }
 
+function isIPv4(address: string): boolean {
+  const pattern = /^((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])$/
+  return pattern.test(address)
+}
+
+function isIPv4CIDR(address: string): boolean {
+  const pattern = /^(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\/([1-9]|[1-2]\d|3[0-2])$/
+  return pattern.test(address)
+}
+
 export {
   isString,
-  isUndefined
+  isUndefined,
+  isIPv4,
+  isIPv4CIDR
 }
