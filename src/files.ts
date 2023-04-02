01a06021f342ff6ca5331db146ec620969731687
fanttasy <470618507@qq.com>
2023-04-02 18:52:11 +0800

    feat: checkFilename

--- src/files.ts
+++ src/files.ts
@@ -1,7 +1,12 @@
-function getExt(filename: string) {
-  return filename.split('.').pop()?.toLocaleLowerCase()
+function getExt(filename: string): string {
+  return filename.split('.').pop()?.toLocaleLowerCase() as string
+}
+
+function checkFilename(filename: string, list: string[]) {
+  return list.includes(getExt(filename))
 }
 
 export {
-  getExt
+  getExt,
+  checkFilename
 }
