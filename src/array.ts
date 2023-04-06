f857576a48228c6a16934bc3006b19cf299177ee
mikasa <59157966+fanttasy@users.noreply.github.com>
2023-04-06 20:02:40 +0800

    feat: sortByProperty

---
+++ src/array.ts
@@ -0,0 +1,22 @@
+function sortByProperty<T>(arr: T[], prop: keyof T, asc = true): T[] {
+  if (!Array.isArray(arr) || arr.length === 0) return [];
+  if (typeof prop !== 'string' || prop.trim() === '') return arr;
+  
+  let sortedArr = arr.slice();
+  
+  sortedArr.sort((a, b) => {
+    let aProp = a[prop];
+    let bProp = b[prop];
+    if (typeof aProp === 'string') aProp = aProp.toLowerCase();
+    if (typeof bProp === 'string') bProp = bProp.toLowerCase();
+    if (aProp < bProp) return asc ? -1 : 1;
+    if (aProp > bProp) return asc ? 1 : -1;
+    return 0;
+  });
+  
+  return sortedArr;
+}
+
+export {
+  sortByProperty
+}
